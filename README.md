7dbd4aa8ce82c9506f2c60e5e212ec70026f9c7f
dune1989 <hafiz.the.ceo@gmail.com>
2017-11-05 09:01:36 +0800

    upload all website content
    reupload all website content

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
-# devcon-website
+# [DevCon : Academia](http://dynamicdune.com/uat/devcon/)
+
+[DevCon : Academia](http://dynamicdune.com/uat/devcon/) is a mock up website develop by Hafiz by using Agency Template from startbootstrap.com. This template is based on bootstrap and jquery and most of the structure has been updated and amend to suits for DevCon : Academia actual and dummy content. There are also a PHP function in which it will send an email to the enquirer whenever they submit the form.
+
+## Screenshots
+
+![alt text](screenshots/sc-1.png "screenshots 1")
+
+![alt text](screenshots/sc-2.png "screenshots 2")
+
+![alt text](screenshots/sc-3.png "screenshots 3")
+
+![alt text](screenshots/sc-4.png "screenshots 4")
+
+![alt text](screenshots/sc-5.png "screenshots 5")
+
+![alt text](screenshots/sc-6.png "screenshots 6")
+
+![alt text](screenshots/sc-7.png "screenshots 7")
+
+![alt text](screenshots/sc-8.png "screenshots 8")
+
+![alt text](screenshots/sc-9.png "screenshots 9")
+
+![alt text](screenshots/sc-10.png "screenshots 10")
+
+**[View Live Preview](https://blackrockdigital.github.io/startbootstrap-agency/)**
+
+## Status
+
+This site is partial completed. If to futher update the content, this readme will be updated for any incoming future update.
---
+++ emailing.php
@@ -0,0 +1,56 @@
+<?php
+//fetch data from enquiry form
+$name = $_POST['fullname'];
+$email = $_POST['email'];
+$phone = $_POST['phone'];
+$message = $_POST['message'];
+
+/*echo $name."<br/>";
+echo $email."<br/>";
+echo $phone."<br/>";
+echo $message."<br/>"; */
+
+$to = $email;
+$from = "admin@devconacademia.com";
+$subject = 'Your Enquiry Has Been Sent';
+
+$headers = "From: " . $from . "\r\n";
+$headers .= "Reply-To: ".$from . "\r\n";
+$headers .= "MIME-Version: 1.0\r\n";
+$headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
+
+$message = '<html><head></head><body>';
+$message .= '<table width="900" align="center" cellpadding="0" cellspacing="0" style="border:3px solid #ddd; font-family: arial, sans-serif;">
+			<thead>
+				<tr>
+					<th  style="text-align:middle; padding-top:25px; padding-bottom:25px;"><h1 style="margin:0; padding:0;"><img src="http://dynamicdune.com/uat/devcon/img/devcon-academia.png" style="width:400px;"></h1></th>
+				</tr>
+				<tr>
+					<th  style="background:#fcfcfc; color: grey; text-align:center; padding-top:30px; padding-bottom:30px;"><h2 style="margin:0; padding:0;">Thank you for sending us an enquiry.</h2></th>
+				</tr>
+			</thead>
+			<tbody>
+				<tr>
+					<td style="color: rgb(165,176,184); padding:25px 35px">
+						<p>Dear '.$name.',</p>
+						<p>This is an automated reply to acknowledge enquirer that their email has been sent. Kindly be patient as we will answer your question soon. For a moment, do enjoy a cup of coffee or be awesome in wherever you be.</p>
+						<p>Wanna know more about our event? Check us out at: <a href="https://www.facebook.com/devconcommunity/" style="color:cornflowerblue;" target="_blank">https://www.facebook.com/devconcommunity/</a></p>
+						<p>Sincerely,<br/>Joe Daft</p>
+					</td>
+				</tr>
+				<tr>
+					<td style="background:#fcfcfc; color: grey; font-size:12px; text-align:center; padding:25px 35px">
+						<span>Devcon Community: Block 3730, Persiaran Apec, Cyberjaya, Selangor 63000, Malaysia </span>
+					</td>
+				</tr>
+			</tbody>
+		</table>';
+$message .= '</body></html>';
+
+// Sending email
+if(mail($to, $subject, $message, $headers)){
+    header('Location: index.php');
+} else{
+    header('Location: index.php');
+}
+?>
